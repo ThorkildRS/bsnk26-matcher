d9b122d8c2cd4ed8f7f2e8d3405c231a5892435b
ThorkildRS <rebostray@gmail.com>
2026-03-04 12:14:32 +0100

    eVAL

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
+    open: true,
+    headers: {
+      'Content-Security-Policy': "script-src 'self' 'unsafe-eval' 'unsafe-inline';"
+    }
   }
 })

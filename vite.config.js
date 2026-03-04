6d0ff22553e3d132f00c495e091a6ec8bccce9c8
ThorkildRS <rebostray@gmail.com>
2026-03-04 12:20:23 +0100

    endring3

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/bsnk26-matcher/',
   server: {
     port: 3000,
-    open: true,
-    headers: {
-      'Content-Security-Policy': "script-src 'self' 'unsafe-eval' 'unsafe-inline';"
-    }
+    open: true
   }
 })
